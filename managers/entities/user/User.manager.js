6921c621c497670d080bc3da473d202eeaa900ab
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2024-05-04 16:27:14 +0300

    Added Login Endpoint

--- managers/entities/user/User.manager.js
+++ managers/entities/user/User.manager.js
@@ -1,13 +1,14 @@
-module.exports = class User { 
+const bcrypt = require("bcrypt");
+module.exports = class User {
 
     constructor({utils, cache, config, cortex, managers, validators, mongomodels }={}){
         this.config              = config;
         this.cortex              = cortex;
-        this.validators          = validators; 
+        this.validators          = validators;
         this.mongomodels         = mongomodels;
         this.tokenManager        = managers.token;
         this.usersCollection     = "users";
-        this.httpExposed         = ['post=register'];
+        this.httpExposed         = ['post=register', 'post=login'];
     }
 
     async register({username, email, password}){
@@ -16,17 +17,28 @@ module.exports = class User {
         // Data validation
         let result = await this.validators.user.register(user);
         if(result) return result;
-        
+
         // Creation Logic
         let createdUser     = new this.mongomodels.user({username, email, password});
         await createdUser.save();
         let longToken       = this.tokenManager.genLongToken({userId: createdUser._id, role: createdUser.role });
-        
+
         // Response
         return {
-            user: createdUser, 
-            longToken 
+            user: createdUser,
+            longToken
         };
+    }
+
+    async login({email, password}){
+        let user = await this.mongomodels.user.findOne({email}).exec();
+        if (!user || !bcrypt.compareSync(password, user.password)) {
+            return {error: "Invalid email/password"}
+        }
+
+        let longToken = this.tokenManager.genLongToken({userId: user._id, role: user.role });
+
+        return {longToken};
     }
 
 }
