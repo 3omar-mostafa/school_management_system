64d6a903d897686288d5b4e3f3d090ebf4e08fa2
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2024-05-04 16:59:08 +0300

    Improved User object serialization

--- managers/entities/user/user.mongoModel.js
+++ managers/entities/user/user.mongoModel.js
@@ -28,4 +28,14 @@ const schema = new mongoose.Schema({
     },
 });
 
+schema.options.toJSON = {
+    transform: function(doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.password;
+        return ret;
+    }
+};
+
 module.exports = mongoose.model("User", schema);
